478bfc6b10b7f7d5d6708d0cbf22e2e270f1308c
collin <collinzizhen@Gmail.com>
2023-01-20 16:31:43 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      test: "hello",
+      quote:''
+
+    }
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(response=> response.json())
+    .then(data => {
+      this.setState({ quote: data.value})});
+  }
+
+  render(){
+    const {quote} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="chucknorris.jpg"  alt="logo" />
+          <p>
+            *{quote}* <br/><br/> <a href="http://localhost:3000/">Refresh</a> to get new chucknorris quote.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
